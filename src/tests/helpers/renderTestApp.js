4da3e09f5e5977a5dc4b608275decfa17e3f219f
Shakuloff <aibekshakulov@gmail.com>
2023-09-07 12:43:32 +0600

    render functions with both of memory router and init redux store

---
+++ src/tests/helpers/renderTestApp.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux"
+import { MemoryRouter } from "react-router-dom"
+import AppRouter from "../../router/AppRouter"
+import { render } from "@testing-library/react"
+import { createReduxStore } from "../../store/store"
+
+export const renderTestApp = (component, options) => {
+    const store = createReduxStore(options?.initialState)
+    return render(
+        <Provider store={store}>
+            <MemoryRouter initialEntries={[options?.route]}>
+                <AppRouter />
+                {component}
+            </MemoryRouter>
+        </Provider>
+    )
+}
