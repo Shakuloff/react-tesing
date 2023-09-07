f87c073a545a5a9f40cd8b47f00ecf745213a609
Shakuloff <aibekshakulov@gmail.com>
2023-09-07 10:50:34 +0600

    page for user detail

---
+++ src/pages/UserDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserDetailPage = () => {
+    return (
+        <div data-testid="user-page">Страница пользователя</div>
+    )
+}
+
+export default UserDetailPage
