d9abd143569cce1aeda2d8d44846980a23166f04
Shakuloff <aibekshakulov@gmail.com>
2023-09-07 11:55:50 +0600

    store counter component

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCounterValue } from '../../store/selectors/getCounterValue/getCounterValue'
+import { decrement, increment } from "../../store/reducers/counterReducer"
+
+const Counter = () => {
+    const dispatch = useDispatch()
+    const value = useSelector(getCounterValue)
+
+    const onIncrement = () => {
+        dispatch(increment())
+    }
+    const onDecrement = () => {
+        dispatch(decrement())
+    }
+
+    return (
+        <div>
+            <h1>значение: {value}</h1>
+            <button onClick={onIncrement}>+</button>
+            <button onClick={onDecrement}>-</button>
+        </div>
+    )
+}
+
+export default Counter
