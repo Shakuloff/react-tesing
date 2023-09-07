99114cce5d4a0601f645b02498d4834478a7dac1
Shakuloff <aibekshakulov@gmail.com>
2023-09-07 10:50:20 +0600

    app navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div>
+            <Link data-testid="main-link" to={'/'}>main</Link>
+            <Link data-testid="about-link" to={'/about'}>about</Link>
+            <Link data-testid="users-link" to={'/users'}>users</Link>
+        </div>
+    )
+}
+
+export default Navbar
