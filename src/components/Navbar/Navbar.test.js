6cb114491b8d4ea9678763465ea179f40f768d72
Shakuloff <aibekshakulov@gmail.com>
2023-09-07 10:53:22 +0600

    test redirecting

---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,29 @@
+import { act } from "react-dom/test-utils"
+import { renderWithRouter } from "../../tests/helpers/RenderWithRouter"
+import Navbar from "./Navbar"
+import { screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+describe("Подтягиввание пользователей меню", () => {
+    test("переходы по всем ссылкам", async () => {
+        await act(async () => {
+            renderWithRouter(<Navbar />)
+        })
+        const mainLink = screen.getByTestId('main-link')
+        const aboutLink = screen.getByTestId('about-link')
+        const usersLink = screen.getByTestId('users-link')
+        
+        await act(async () => {
+            userEvent.click(aboutLink)
+        })
+        expect(screen.getByTestId('about-page')).toBeInTheDocument()
+        await act(async () => {
+            userEvent.click(mainLink)
+        })
+        expect(screen.getByTestId('main-page')).toBeInTheDocument()
+        await act(async () => {
+            userEvent.click(usersLink)
+        })
+        expect(screen.getByTestId('users-page')).toBeInTheDocument()
+    })
+})
