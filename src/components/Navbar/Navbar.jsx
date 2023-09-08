449fd463fc703ff46303a09ea222b65faa540cfe
Shakuloff <aibekshakulov@gmail.com>
2023-09-08 17:01:45 +0600

    add link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     return (
         <div>
             <Link data-testid="main-link" to={'/'}>main</Link>
+            <Link data-testid="privet-link" to={'/privet'}>privet</Link>
             <Link data-testid="about-link" to={'/about'}>about</Link>
             <Link data-testid="users-link" to={'/users'}>users</Link>
         </div>
