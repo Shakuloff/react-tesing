9d301bb82e4c2d58c9c91bbf3c2d1bae9c79a74d
Shakuloff <aibekshakulov@gmail.com>
2023-09-08 17:01:39 +0600

    add routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,13 +5,16 @@ import AboutPage from '../pages/AboutPage'
 import Users from '../users/Users'
 import UserDetailPage from '../pages/UserDetailPage'
 import ErrorPage from '../pages/ErrorPage'
-
+import PrivetMir from '../pages/PrivetMir'
+import UsersForTest from '../components/usersForTest/UsersForTest'
 const AppRouter = () => {
     return (
         <Routes>
             <Route path='/' element={<MainPage />}></Route>
+            <Route path='/privet' element={<PrivetMir />}></Route>
             <Route path='/about' element={<AboutPage />}></Route>
             <Route path='/users' element={<Users />}></Route>
+            <Route path='/users-test' element={<UsersForTest />}></Route>
             <Route path='/users/:id' element={<UserDetailPage />}></Route>
             <Route path='/*' element={<ErrorPage />}></Route>
         </Routes>
